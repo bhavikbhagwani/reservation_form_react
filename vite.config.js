f8a7ffe0b44485db9d57efa625e6e264292765c9
bhavik <bhavik.bhagwani0001@stud.hkr.se>
2024-07-19 01:16:15 +0200

    try

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/res_page/",
+  base: "https://bhavikbhagwani.github.io/reservation_form_react",
   plugins: [react()],
 })
